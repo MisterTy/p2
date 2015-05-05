7e6a2ca7e5ff71d9e47529a1f33f5d98cebe0465
K3ller <Dominik@macbook.edu.ds.fhnw.ch>
2015-05-05 11:15:17 +0200

    PIRegler fertig

--- src/PIRegler.java
+++ src/PIRegler.java
@@ -2,59 +2,180 @@
 import java.util.*;
 
 import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.util.FastMath;
 
 /**
- * 
+ * Diese Klasse berechnet die Parameter eines PI-Reglers nach dem Zellwegerverfahren.
+ * @author Dominik
+ *
  */
 public class PIRegler extends GenericRegler {
-	Complex[] Gs;
-	double[] phi_s; 
-	double[] w;
-	double Tn;
-	Complex[] Grp;
+	
+	
+	private double[] phi_s; 
+	private double[] w;
+	private double[] phi_rprov;
+	private double[] phi_0;
+	private double[] t;
+	
+	private double tn;
+	private double tv;
+	private double tp;
+	private double kS;
+	private double kR;
+	private double phir;
+	private double wD;
+	
+	private Complex[] grp;
+	private Complex[] gs;
+	
+	private Complex grp_wd;
+	private Complex g_str_wd;
+	private Complex gOffwd;
 
-   /**
-    * 
-    * @param Gs
-    * @param w
-    * @param phir
-    * @param kS
-    * @param T
-    * @return[kR,Tn,Tv,Tp]
-    */
-    public PIRegler(Complex[] Gs, double w[],double phir,double kS, double[] T) {
+  /**
+   * Konstruktor des PI Reglers
+   * 
+   * @param gs
+   * @param w
+   * @param phir
+   * @param kS
+   * @param T
+   */
+    public PIRegler(Complex[] gs, double w[],double phir,double kS, double[] T) {
 
-    	this.Gs=Gs;
+    	int lGs=gs.length;
+    	this.gs=gs;
     	this.w=w;
-    	phi_s= new double[Gs.length];
-    	Grp=new Complex[Gs.length];
+    	this.phir=phir;
+    	this.t=T;
+    	this.kS=kS;
     	
+    	tv=-1;
+    	tp=-1;
     	
+    	phi_s= new double[lGs];
+    	phi_rprov= new double[lGs];
+    	phi_0= new double[lGs];
+    	grp= new Complex[lGs];
     	
-    }
+  }
 
-    public void berechnungTn(){
+    
+    private void berechnungTn(){
     	
     	
-    	for (int i = 0; i < Gs.length; i++)  phi_s[i] = Gs[i].getArgument();
-    	for (int y = 0; y < Gs.length; y++) {
+    	for (int i = 0; i < gs.length; i++)  phi_s[i] = gs[i].getArgument();
+    	
+    	
+
+    	for (int y = 0; y < phi_s.length; y++) {
     		if(phi_s[y]>0){
-    			for(int z=y;y < Gs.length; z++)	phi_s[z]=-2*Math.PI+phi_s[z];
-    		}			
+    			for(int z=y;z < phi_s.length; z++)	phi_s[z]=-2*Math.PI+phi_s[z];
+    			break;
+    		}   		
 		}
     	
-    	int ind_left=MathLibrary.int_ver(phi_s, -(Math.PI/2))[0];
-    	int ind_right=MathLibrary.int_ver(phi_s, -(Math.PI/2))[1];
+    	
+    	int ind_left=MathLibrary.int_ver(phi_s, -(1.57079))[0];
+    	int ind_right=MathLibrary.int_ver(phi_s, -(1.57079))[1];
     	
     	double wpi=(w[ind_left]+w[ind_right])/2;
-    	Tn=1/wpi;
+    	tn=1/wpi;
    	
        	for (int i = 0; i < w.length; i++) {
-			Grp[i]=(Complex.I.multiply(w[i]).multiply(Tn)).reciprocal().add(1); //Übertragungsfkt PI Regeler mit kR=1
+			grp[i]=(Complex.I.multiply(w[i]).multiply(tn)).reciprocal().add(1); //Übertragungsfkt PI Regeler mit kR=1
 		}
-       	System.out.println("Tn: "+Tn);
-       	System.out.println("Grp "+Arrays.toString(Grp));
  	}
+    
+    private void berechnungWD(){
+    	for (int i = 0; i < grp.length; i++) phi_rprov[i]=grp[i].getArgument();
+    	for (int i = 0; i < grp.length; i++) phi_0[i]=phi_s[i]+phi_rprov[i];
+    	
+    	for (int y = 0; y < phi_0.length; y++) {
+    		if(phi_s[y]>0){
+    			for(int z=y;z < phi_0.length; z++)	phi_0[z]=-2*Math.PI+phi_0[z];
+    			break;
+    		}   		
+		}
+    	int ind_left=MathLibrary.int_ver(phi_0, -Math.PI+phir)[0];
+    	int ind_right=MathLibrary.int_ver(phi_0, -Math.PI+phir)[1];
+    	wD=((w[ind_left]+w[ind_right])/2);
+    	grp_wd=(Complex.I.multiply(wD).multiply(tn).reciprocal()).add(1);
+    }
+    
+    private void berechnungStrecke(){
+    	g_str_wd=Complex.ONE;
+    	for (int i = 0; i < t.length; i++) g_str_wd=((Complex.I.multiply(wD).multiply(t[i]).add(1)).reciprocal()).multiply(g_str_wd);			    	
+    	g_str_wd=g_str_wd.multiply(kS);
+   }
+    
+    private void berechnungOffenerRegelkreis(){
+    	gOffwd=grp_wd.multiply(g_str_wd);
+    	double amplOffwd=20*FastMath.log10(gOffwd.abs());
+    	double kRdb=-amplOffwd;
+    	kR=FastMath.pow(10, (kRdb/20));
+    	gOffwd=grp_wd.multiply(g_str_wd);
+    }
+    /**
+     * Gibt Werte des PI Reglers in der Konsole aus.
+     */
+    public void konsoleOut(){
+    	System.out.println("kR: "+kR);
+    	System.out.println("Tn: "+tn);
+    	System.out.println("Tv:" +tv);
+    	System.out.println("Tp:" +tp);
+    }
+    /**
+     * Führt die Berechnung des PI Reglers durch
+     */
+    public void compute(){
+    	berechnungTn();
+    	berechnungWD();
+    	berechnungStrecke();
+    	berechnungOffenerRegelkreis();
+    	
+    }
+    
+    /**
+     * Neue Gs Liste übergeben
+     * @param gs
+     */
+    public void setGs(Complex[] gs){
+    	this.gs=gs;
+    }
+    
+    /**
+     * Neue Omega Liste übergeben
+     * @param w
+     */
+    public void setW(double[] w){
+    	this.w=w;
+    }
+    /**
+     * Neuer Phasenrand übergeben
+     * @param phir
+     */
+    public void setPhir(double phir){
+    	this.phir=phir;
+    }
+    
+    /**
+     * Neue Verstärkung übergeben
+     * @param kS
+     */
+    public void setkS(double kS){
+    	this.kS=kS;
+    }
+    /**
+     * Neue Zeitkonstanten übergeben
+     * @param t
+     */
+    public void setT(double[] t){
+    	this.t=t;
+    }
+    
+    
     
     
 
