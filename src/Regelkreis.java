5036d1d237624a698830575e65f7c4b8b34b08cf
MisterTy <timon.ziltener@inbox.com>
2015-05-07 13:23:07 +0200

    Regelkreis vergessen bei vorherigem Commit

--- src/Regelkreis.java
+++ src/Regelkreis.java
@@ -9,27 +9,37 @@ import org.apache.commons.math3.complex.Complex;
 public class Regelkreis {
 	
 	GenericRegler regler;
+	StepResponse stepResponse = new StepResponse();
 
     /**
      * 
      */
     public Regelkreis(int type, Complex[] utfStrecke, double[] kreisFrequenzSpektrum, double phasenrand, double verstarkungStrecke, double[] zeitkonstante){
-    	
     	switch (type){
 		case Model.piRegler:
 			regler = new PIRegler();
 			regler.reglerTyp = Model.piRegler;
 			regler.setValues(utfStrecke, kreisFrequenzSpektrum, phasenrand, verstarkungStrecke, zeitkonstante);
 			regler.compute();
+			// Befehl Schrittantwort berechnen
 			break;
 		case Model.pidRegler:
 			regler = new PIDRegler();
 			regler.reglerTyp = Model.pidRegler;
 			regler.setValues(utfStrecke, kreisFrequenzSpektrum, phasenrand, verstarkungStrecke, zeitkonstante);
 			regler.compute();
+			// Befehl Schrittantwort berechnen
 			break;
     	}
-    	
+    	stepResponse.calc(regler.reglerTyp, regler.getResult(), verstarkungStrecke, zeitkonstante, kreisFrequenzSpektrum);
+    }
+    
+    public double[] getXValues(){
+    	return stepResponse.gettAxis();
+    }
+    
+    public double[] getYValues(){
+    	return stepResponse.getyAxis();
     }
     
     public void output(){
